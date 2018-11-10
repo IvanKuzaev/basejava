ed5f7b31d9fb5c079bdc7b1546b91a6bcc4a2b11
Ivan Kuzaev <34812749+IvanKuzaev@users.noreply.github.com>
2018-11-10 20:12:24 +0400

    Method clear() fills storage array with nulls. Method find() does return value directly. Some style changes.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,44 +7,47 @@ public class ArrayStorage {
     private int size;
 
     void clear() {
-        //storage = null;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
         size = 0;
     }
 
     void save(Resume r) {
         storage[size] = r;
-        size += 1;
+        size++;
     }
 
     private int find(String uuid) {
-        int r = -1;
-        for (int i = 0; i < size; i += 1)
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                r = i;
-                break;
+                return i;
             }
-        return r;
+        }
+        return -1;
     }
 
     private void shift(int i) {
-        for (; storage[i + 1] != null; i += 1)
+        for (; storage[i + 1] != null; i++) {
             storage[i] = storage[i + 1];
+        }
     }
 
     Resume get(String uuid) {
         int i = find(uuid);
-        if (i > -1)
+        if (i > -1) {
             return storage[i];
-        else
+        }
+        else {
             return null;
+        }
     }
 
     void delete(String uuid) {
         int i = find(uuid);
         if (i > -1) {
-            //storage[i] = null;
             shift(i);
-            size -= 1;
+            size--;
         }
     }
 
@@ -53,8 +56,9 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         Resume[] all = new Resume[size];
-        for (int i = 0; i < size; i += 1)
+        for (int i = 0; i < size; i++) {
             all[i] = storage[i];
+        }
         return all;
     }
 
