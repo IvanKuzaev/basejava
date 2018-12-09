69a1b2ba8cc51f226f5a8fa4e37f92ef23843433
IvanKuzaev <ivkuzaev@mail.ru>
2018-12-09 21:37:53 +0300

    Resume class: uuid field made private, getters/setters added. ArrayStorage class: method delete() rewritten, simplified algorithm used; method shift() - more accurate algorithm implemented; method find() renamed to getIndex(). MainArray and MainTestArrayStorage classes: tests of method update() added.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,92 +1,115 @@
 /**
  * Array based storage for Resumes
  */
+
 import java.util.Arrays;
 
 public class ArrayStorage {
 
-    static private final int MAX = 10000;
+    static private final int MAX = 10_000;
 
     Resume[] storage = new Resume[MAX];
 
     private int size;
 
+    /**
+     * clears storage by erasing all object references
+     */
     void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    private int find(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
+    /**
+     * adds a new resume to the and of storage
+     */
     void save(Resume r) {
-        int i = find(r.uuid);
+        int i = getIndex(r.getUuid());
         if (i == -1) {
             if (size < MAX) {
                 storage[size] = r;
                 size++;
-            }
-            else {
+            } else {
                 System.out.println("save() method: storage array overflow.");
             }
-        }
-        else {
-            System.out.println("save() method: resume "+r.uuid+" is in database, use update() method.");
-        }
-    }
-
-    private void shift(int i) {
-        for (; storage[i + 1] != null; i++) {
-            storage[i] = storage[i + 1];
+        } else {
+            System.out.println("save() method: resume " + r.getUuid() + " is in database, use update() method.");
         }
     }
 
+    /**
+     * replaces a resume in storage with a new one with the same uuid
+     */
     void update(Resume r) {
-        int i = find(r.uuid);
+        int i = getIndex(r.getUuid());
         if (i > -1) {
             storage[i] = r;
-        }
-        else {
-            System.out.println("update() method: resume "+r.uuid+" not found.");
+        } else {
+            System.out.println("update() method: resume " + r.getUuid() + " not found.");
         }
     }
 
+    /**
+     * @return Resume, instance of Resume with specified uuid, if not found, returns null
+     */
     Resume get(String uuid) {
-        int i = find(uuid);
+        int i = getIndex(uuid);
         if (i > -1) {
             return storage[i];
-        }
-        else {
-            System.out.println("get() method: resume "+uuid+" not found.");
+        } else {
+            System.out.println("get() method: resume " + uuid + " not found.");
             return null;
         }
     }
 
+    /**
+     * deletes resume with particular uuid if found, doesn't preserve order of elements
+     */
     void delete(String uuid) {
-        int i = find(uuid);
+        int i = getIndex(uuid);
         if (i > -1) {
-            shift(i);
+            if (i < size - 1) {
+                storage[i] = storage[size - 1];
+            }
+            storage[size - 1] = null;
             size--;
+        } else {
+            System.out.println("delete() method: resume " + uuid + " not found.");
         }
-        else {
-            System.out.println("delete() method: resume "+uuid+" not found.");
+    }
+
+    //use this method to delete an element if order is relevant
+    private void shift(int i) {
+        for (; (storage[i + 1] != null) & (i + 1 < MAX); i++) {
+            storage[i] = storage[i + 1];
         }
+        storage[i] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.<Resume>copyOf(storage, size);
+        return Arrays.copyOf(storage, size);
     }
 
+    /**
+     * @return int, amount of resumes in storage, also the next free index to fill
+     */
     int size() {
         return size;
     }
+
+    /**
+     * @return int, index in storage of found resume, if no resume found, returns -1
+     */
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
