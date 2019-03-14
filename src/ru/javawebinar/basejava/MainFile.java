4130f80820173c2342bb67fcfed0672b37048c25
Ivan Kuzaev <34812749+IvanKuzaev@users.noreply.github.com>
2019-03-15 00:32:10 +0400

    Method MainFile.recursivePrintDirectoryStructure() is simplified.

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,31 +1,20 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.util.ArrayList;
 
 public class MainFile {
 
-    public static void recursivePrintDirectoryStructure(String tabs, File directory) {
-        if (directory.isDirectory()) {
-            System.out.println(tabs + "[" + directory.getName() + "]");
-            File[] entries = directory.listFiles();
+    public static void recursivePrintDirectoryStructure(String tabs, File entry) {
+        if (entry.isDirectory()) {
+            System.out.println(tabs + "[" + entry.getName() + "]");
+            File[] entries = entry.listFiles();
             if (entries != null) {
-                ArrayList<File> dirs = new ArrayList<>();
-                ArrayList<File> files = new ArrayList<>();
                 for (File e : entries) {
-                    if (e.isDirectory()) {
-                        dirs.add(e);
-                    } else {
-                        files.add(e);
-                    }
-                }
-                for (int i = 0; i < dirs.size(); i++) {
-                    recursivePrintDirectoryStructure(tabs + "\t", dirs.get(i));
-                }
-                for (int i = 0; i < files.size(); i++) {
-                    System.out.println(tabs + "\t" + files.get(i).getName());
+                    recursivePrintDirectoryStructure(tabs + "\t", e);
                 }
             }
+        } else {
+            System.out.println(tabs + entry.getName());
         }
     }
 
