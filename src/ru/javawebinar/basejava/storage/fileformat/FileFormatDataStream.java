e5fd89e9089ab6f00b3b5ffc500638c1494a148e
Ivan Kuzaev <34812749+IvanKuzaev@users.noreply.github.com>
2019-03-31 07:53:34 +0400

    HW10 update: Generic methods for writing/reading collections added.

--- src/ru/javawebinar/basejava/storage/fileformat/FileFormatDataStream.java
+++ src/ru/javawebinar/basejava/storage/fileformat/FileFormatDataStream.java
@@ -8,75 +8,123 @@ import java.util.*;
 
 public class FileFormatDataStream implements FileFormatStrategy {
 
-    private void writeStringSection (DataOutputStream dos, StringSection stringSection) throws IOException {
+    private interface WriteItem<T>{
+        void write(T t) throws IOException;
+    }
+
+    private <T> void writeCollection(DataOutputStream dos, Collection<T> collection, WriteItem<T> item) throws IOException {
+        dos.writeInt(collection.size());
+        for(T t : collection) {
+            item.write(t);
+        }
+    }
+
+    private interface ReadItem<T>{
+        T read() throws IOException;
+        default Collection<T> collection() {
+            return new ArrayList<T>();
+        }
+    }
+
+    private <T> Collection<T> readCollection(DataInputStream dis, ReadItem<T> item) throws IOException {
+        Collection<T> collection = item.collection();
+        int itemCount = dis.readInt();
+        for (int i = 0; i < itemCount; i++) {
+            collection.add(item.read());
+        }
+        return collection;
+    }
+
+    private void writeStringSection(DataOutputStream dos, StringSection stringSection) throws IOException {
         dos.writeUTF(stringSection.getData());
     }
 
-    private StringSection readStringSection (DataInputStream dis) throws IOException {
+    private StringSection readStringSection(DataInputStream dis) throws IOException {
         return new StringSection(dis.readUTF());
     }
 
-    private void writeStringListSection (DataOutputStream dos, StringListSection stringListSection) throws IOException {
-        dos.writeInt(stringListSection.size());
-        for (String string : stringListSection.getData()) {
-            dos.writeUTF(string);
-        }
+    private void writeStringListSection(DataOutputStream dos, StringListSection stringListSection) throws IOException {
+        writeCollection(dos, stringListSection.getData(), dos::writeUTF);
     }
 
-    private StringListSection readStringListSection (DataInputStream dis) throws IOException {
-        List<String> listString = new ArrayList<String>();
-        int stringCount = dis.readInt();
-        for (int i = 0; i < stringCount; i++) {
-            listString.add(dis.readUTF());
-        }
-        return new StringListSection(listString);
+    private StringListSection readStringListSection(DataInputStream dis) throws IOException {
+        return new StringListSection((List<String>)readCollection(dis, dis::readUTF));
     }
 
-    private void writeBioSection (DataOutputStream dos, BioSection bioSection) throws IOException {
-        dos.writeInt(bioSection.size());
-        for (LifeStage lifeStage : bioSection.getData()) {
-            Organization organization = lifeStage.getOrganization();
-            dos.writeUTF(Objects.requireNonNullElse(organization.getTitle(), ""));
-            dos.writeUTF(Objects.requireNonNullElse(organization.getDescription(), ""));
-            dos.writeUTF(Objects.requireNonNullElse(organization.getWebLink(), ""));
-            dos.writeInt(lifeStage.size());
-            for (LifePeriod lifePeriod : lifeStage.getData()) {
-                dos.writeInt(lifePeriod.getStartDate().getYear());
-                dos.writeInt(lifePeriod.getStartDate().getMonthValue());
-                dos.writeInt(lifePeriod.getEndDate().getYear());
-                dos.writeInt(lifePeriod.getEndDate().getMonthValue());
-                dos.writeUTF(Objects.requireNonNullElse(lifePeriod.getTitle(), ""));
-                dos.writeUTF(Objects.requireNonNullElse(lifePeriod.getDescription(), ""));
-            }
-        }
+    private void writeLocalDate(DataOutputStream dos, LocalDate localDate) throws IOException {
+        dos.writeInt(localDate.getYear());
+        dos.writeInt(localDate.getMonthValue());
     }
 
-    private BioSection readBioSection (DataInputStream dis) throws IOException {
-        List<LifeStage> listLifeStage = new ArrayList<LifeStage>();
-        int lifeStageCount = dis.readInt();
-        for (int i = 0 ; i < lifeStageCount; i++) {
-            String title = dis.readUTF();
-            title = title.length() > 0 ? title : null;
-            String description = dis.readUTF();
-            description = description.length() > 0 ? description : null;
-            String webLink = dis.readUTF();
-            webLink = webLink.length() > 0 ? webLink : null;
-            Organization organization = new Organization(title, description, webLink);
-            List<LifePeriod> listLifePeriod = new ArrayList<LifePeriod>();
-            int lifePeriodCount = dis.readInt();
-            for(int j = 0; j < lifePeriodCount; j++) {
-                LocalDate startDate = LocalDate.of(dis.readInt(), dis.readInt(), 1);
-                LocalDate endDate = LocalDate.of(dis.readInt(), dis.readInt(), 1);
-                title = dis.readUTF();
-                title = title.length() > 0 ? title : null;
-                description = dis.readUTF();
-                description = description.length() > 0 ? description : null;
-                LifePeriod lifePeriod = new LifePeriod(startDate, endDate, title, description);
-                listLifePeriod.add(lifePeriod);
-            }
-            listLifeStage.add(new LifeStage(organization, listLifePeriod));
-        }
+    private LocalDate readLocalDate(DataInputStream dis) throws IOException {
+        return LocalDate.of(dis.readInt(), dis.readInt(), 1);
+    }
+
+    private void writeString(DataOutputStream dos, String string) throws IOException {
+        dos.writeUTF(Objects.requireNonNullElse(string, ""));
+    }
+
+    private String readString(DataInputStream dis) throws IOException {
+        String string = dis.readUTF();
+        return string.length() > 0 ? string : null;
+    }
+
+    private void writeLifePeriod(DataOutputStream dos, LifePeriod lifePeriod) throws IOException {
+        writeLocalDate(dos, lifePeriod.getStartDate());
+        writeLocalDate(dos, lifePeriod.getEndDate());
+        writeString(dos, lifePeriod.getTitle());
+        writeString(dos, lifePeriod.getDescription());
+    }
+
+    private LifePeriod readLifePeriod(DataInputStream dis) throws IOException {
+        LocalDate startDate = readLocalDate(dis);
+        LocalDate endDate = readLocalDate(dis);
+        String title = readString(dis);
+        String description = readString(dis);
+        return new LifePeriod(startDate, endDate, title, description);
+    }
+
+    private void writeLifeStage(DataOutputStream dos, LifeStage lifeStage) throws IOException {
+        Organization organization = lifeStage.getOrganization();
+        writeString(dos, organization.getTitle());
+        writeString(dos, organization.getDescription());
+        writeString(dos, organization.getWebLink());
+        writeCollection(dos, lifeStage.getData(), lp -> writeLifePeriod(dos, lp));
+    }
+
+    private LifeStage readLifeStage(DataInputStream dis) throws IOException {
+        String title = readString(dis);
+        String description = readString(dis);
+        String webLink = readString(dis);
+        Organization organization = new Organization(title, description, webLink);
+        List<LifePeriod> listLifePeriod = (List<LifePeriod>)readCollection(dis, () -> readLifePeriod(dis));
+        return new LifeStage(organization, listLifePeriod);
+    }
+
+    private void writeBioSection(DataOutputStream dos, BioSection bioSection) throws IOException {
+        writeCollection(dos, bioSection.getData(), ls -> writeLifeStage(dos, ls));
+    }
+
+    private BioSection readBioSection(DataInputStream dis) throws IOException {
+        List<LifeStage> listLifeStage = (List<LifeStage>)readCollection(dis, () -> readLifeStage(dis));
         return new BioSection(listLifeStage);
+    }
+
+    void writeSection(DataOutputStream dos, Resume resume, Sections section) throws IOException {
+        switch (section) {
+            case OBJECTIVE:
+            case PERSONAL:
+                writeStringSection(dos, (StringSection)resume.getSection(section));
+                break;
+            case ACHIEVEMENTS:
+            case QUALIFICATIONS:
+                writeStringListSection(dos, (StringListSection)resume.getSection(section));
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                writeBioSection(dos, (BioSection)resume.getSection(section));
+                break;
+        }
     }
 
     @Override
@@ -84,18 +132,19 @@ public class FileFormatDataStream implements FileFormatStrategy {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
-            Map<Contacts, String> contacts = resume.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<Contacts, String> entry : contacts.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(entry.getValue());
-            }
-            writeStringSection(dos, (StringSection)resume.getSection(Sections.OBJECTIVE));
-            writeStringSection(dos, (StringSection)resume.getSection(Sections.PERSONAL));
-            writeStringListSection(dos, (StringListSection)resume.getSection(Sections.ACHIEVEMENTS));
-            writeStringListSection(dos, (StringListSection)resume.getSection(Sections.QUALIFICATIONS));
-            writeBioSection(dos, (BioSection)resume.getSection(Sections.EXPERIENCE));
-            writeBioSection(dos, (BioSection)resume.getSection(Sections.EDUCATION));
+            writeCollection(dos, resume.getContacts().entrySet(), (e) -> { dos.writeUTF(e.getKey().name()); writeString(dos, e.getValue());});
+            writeSection(dos, resume, Sections.OBJECTIVE);
+            writeSection(dos, resume, Sections.PERSONAL);
+            writeSection(dos, resume, Sections.ACHIEVEMENTS);
+            writeSection(dos, resume, Sections.QUALIFICATIONS);
+            writeSection(dos, resume, Sections.EXPERIENCE);
+            writeSection(dos, resume, Sections.EDUCATION);
+//            writeStringSection(dos, (StringSection)resume.getSection(Sections.OBJECTIVE));
+//            writeStringSection(dos, (StringSection)resume.getSection(Sections.PERSONAL));
+//            writeStringListSection(dos, (StringListSection)resume.getSection(Sections.ACHIEVEMENTS));
+//            writeStringListSection(dos, (StringListSection)resume.getSection(Sections.QUALIFICATIONS));
+//            writeBioSection(dos, (BioSection)resume.getSection(Sections.EXPERIENCE));
+//            writeBioSection(dos, (BioSection)resume.getSection(Sections.EDUCATION));
         }
     }
 
@@ -118,4 +167,5 @@ public class FileFormatDataStream implements FileFormatStrategy {
             return resume;
         }
     }
+
 }
